05df1a158856acf7241e46b829cd717e3c249790
Sergio Gordillo <sgordillogallardo@gmail.com>
2017-06-26 13:15:29 +0200

    Open project

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gef-cli',
-      version='0.4.3',
+      version='1.0.0',
       description='Library to interact with GEF',
       author='Sergio Gordillo, Raul Requero',
       author_email='sergio.gordillo@vizzuality.com,raul.requero@vizzuality.com',
